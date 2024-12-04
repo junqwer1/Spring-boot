329410c15e83d226c64e87c61e0cb88f051c0794
junqwer1 <163124941+junqwer1@users.noreply.github.com>
2024-12-04 13:20:39 +0900

    .

--- exam05/src/test/java/org/koreait/rests/Ex01.java
+++ exam05/src/test/java/org/koreait/rests/Ex01.java
@@ -1,11 +1,15 @@
 package org.koreait.rests;
 
+import org.hibernate.type.TrueFalseConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.koreait.member.controllers.ApiMemberController;
+import org.koreait.member.controllers.RequestLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,12 +33,38 @@ public class Ex01 {
 
     @BeforeEach
     void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+//        모든 컨트롤러 빈을 생성하고 테스트
+//        mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+//        ApiMemberController 빈만 생성하고 테스트
+        mockMvc = MockMvcBuilders.standaloneSetup(ApiMemberController.class).build();
     }
 
     @Test
     void test1() throws Exception{
         mockMvc.perform(get("/api/member/test1"))
                 .andDo(print());
+    }
+
+    @Test
+    void test2() throws Exception{
+        mockMvc.perform(post("/api/member/test3")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED) //application/x-www-form-urlencoded
+                .param("email", "user01@test.org")
+                .param("password", "12345678")
+                .param("saveEmail", "true")
+        ).andDo(print());
+
+        /*
+        * 요청 바디 데이터
+        * email =user01@test.org&password=12345678&saveEmail=true
+        * */
+    }
+
+    @Test
+    void test3() throws Exception {
+        RequestLogin form = new RequestLogin();
+        form.setEmail("user01@test.org");
+        form.setPassword("12345678");
+        form.setSaveEmail(true);
     }
 }
